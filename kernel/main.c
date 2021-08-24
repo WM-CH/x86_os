5a87422f26ad39d151baa5b024544d9a613af2fa
Eason <shen5773384@163.com>
2021-08-24 23:40:57 +0800

    键盘驱动

--- kernel/main.c
+++ kernel/main.c
@@ -4,6 +4,10 @@
 #include "interrupt.h"
 #include "console.h"
 
+/* 临时为测试添加 */
+#include "ioqueue.h"
+#include "keyboard.h"
+
 void k_thread_a(void*);
 void k_thread_b(void*);
 /*
@@ -25,30 +29,37 @@ int main(void) {
 	put_str("I am kernel\n");
 	init_all();
 	
-	// kernel线程优先级也是31 所以打印个数"Main"和"argA"相同，是"argB"的4倍
-	thread_start("k_thread_a", 31, k_thread_a, "argA ");
-	thread_start("k_thread_b", 8, k_thread_b, "argB ");
-
-	intr_enable();	// 打开中断,使时钟中断起作用
-	while(1) {
-		console_put_str("Main ");
-	};
+	thread_start("consumer_a", 31, k_thread_a, " A_");
+	thread_start("consumer_b", 31, k_thread_b, " B_");
+	intr_enable();
+	while(1);
+	
 	return 0;
 }
 
 /* 在线程中运行的函数 */
 void k_thread_a(void* arg) {
-	char* para = arg;
 	while(1) {
-		console_put_str(para);
+		enum intr_status old_status = intr_disable();
+		if (!ioq_empty(&kbd_buf)) {
+			console_put_str(arg);
+			char byte = ioq_getchar(&kbd_buf);
+			console_put_char(byte);
+		}
+		intr_set_status(old_status);
 	}
 }
 
 /* 在线程中运行的函数 */
 void k_thread_b(void* arg) {
-	char* para = arg;
 	while(1) {
-		console_put_str(para);
+		enum intr_status old_status = intr_disable();
+		if (!ioq_empty(&kbd_buf)) {
+			console_put_str(arg);
+			char byte = ioq_getchar(&kbd_buf);
+			console_put_char(byte);
+		}
+		intr_set_status(old_status);
 	}
 }
 
